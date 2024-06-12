3d59a91a3110a68de13047a5547a2f4c776b9db3
Johnny Hall <johnny.hall@paramet.co.uk>
2024-06-12 12:57:28 +0100

    setup Column as droppable

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,31 @@ import './App.css'
 import { initialData } from './initial-data'
 import { useState } from 'react';
 import Column from './Column';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 function App() {
 
   const [state, setState] = useState(initialData);
 
+  const onDragEnd = () => {
+    // TODO reorders the column items
+    console.log('element was dropped')
+  };
 
   return (
     <>
+      <DragDropContext
+        onDragEnd={onDragEnd}
+      >
+        {state.columnOrder.map((columnId => {
 
-      {state.columnOrder.map((columnId => {
+          const column = state.columns[columnId];
+          const foods = column.foodIds.map(foodId => state.foods[foodId]);
 
-        const column = state.columns[columnId];
-        const foods = column.foodIds.map(foodId => state.foods[foodId]);
+          return <Column key={column.id} column={column} foods={foods} />
 
-        return <Column key={column.id} column={column} foods={foods} />
-
-      }))}
+        }))}
+      </DragDropContext>
     </>
   )
 }
--- src/Column.jsx
+++ src/Column.jsx
@@ -1,23 +1,37 @@
 import './App.css'
 import Food from './Food'
+import { Droppable } from 'react-beautiful-dnd';
 
-function Column({ key, column, foods }) {
+function Column({ column, foods }) {
 
     const totalCalories = foods.reduce((total, food) => total + food.calories, 0);
 
 
     return (
+
         <div className=" m-2 border-black border-2 w-60 flex flex-col justify-items-center">
             <h3 className="p-2 bg-green-50 text-center">{column.title}</h3>
-            <div className="p-2">
-                {foods.map(food => <Food key={food.id} food={food} />)}
-            </div>
+
+            <Droppable droppableId={column.id}>
+                {provided => {
+                    return (
+                        <div className="p-2"
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            {foods.map(food => <Food key={food.id} food={food} />)}
+                            {provided.placeholder}
+                        </div>
+                    )
+                }}
+            </Droppable>
 
             <div className="p-2 bg-red-50 flex flex-row justify-around">
                 <p>TOTAL CALS:</p>
                 <p>{totalCalories}</p>
             </div>
         </div>
+
     )
 }
 
